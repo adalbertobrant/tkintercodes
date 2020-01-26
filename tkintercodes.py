838a2af1ebaca6e5d3950f17280ca6d072740372
adalbertobrant <adalbertobrant@gmail.com>
2020-01-26 07:32:12 -0300

    Initial commit - Simple tkinter program using class

---
+++ tkintercodes.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# class window , need to be called in the main
+class window():
+    def __init__(self):
+        self.wdw = Tk() # create Tk() instance
+
+        self.wdw.mainloop() # continuous loop
+
+# call window()
+root = window()
+
+
+
+
