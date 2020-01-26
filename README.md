93a692f5ad483d4b0933e67b62c0aaa44aeddbf2
adalbertobrant <adalbertobrant@gmail.com>
2020-01-26 07:57:34 -0300

    Atualizando o código, e fazendo o README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+<h3> Small Codes to help in tkinter development.</h3>
+
+<table style = "width:100%">
+<tr>
+<th>Code Name</th>
+<th> Description</th>
+</tr>
+<tr>
+<td>
+tkintercodes.py
+</td><td>Main Window -Class format with title</td></tr>
--- tkintercodes.py
+++ tkintercodes.py
@@ -1,15 +1,22 @@
 from tkinter import *
 
-# class window , need to be called in the main
-class window():
-    def __init__(self):
-        self.wdw = Tk() # create Tk() instance
+# Classe Janela com a propriedade title -> título da janela
+class Janela:
+    def __init__(self,mestre):
+# liga o self.mestre -> mestre
+        self.mestre = mestre
+        mestre.title("Main Window")
 
-        self.wdw.mainloop() # continuous loop
-
-# call window()
-root = window()
-
+# cria a variável root que tem as propriedades da classe Tk()
+root = Tk()
+# imprime na tela o tipo de root - root é do tipo classe tkinter.tk
+print(type(root))
+wdw = Janela(root)
+# propriedade __main__ -> da classe Janela herdada da classe Tk()
+print(type(wdw))
+# mudança do título que estava dentro da classe Janela
+root.title("New Title")
+root.mainloop()
 
 
 
